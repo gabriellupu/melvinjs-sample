f831f213d932950d7d1e284486aa975df9ca7024
Gabriel Lupu <gabriel.lupu@codegile.com>
2017-01-30 13:25:26 -0800

    Update library versions according to last changes to melvinjs-grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,28 @@
 var grunt = require('grunt');
+var package_json = require('./package.json');
 
 module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-melvinjs');
 
     /**
-     *  Extend grunt tasks. You can add your own stuff here:
+     *  Extend or override already defined grunt tasks
+     *  This example overrides the production ulglify task by adding
+     *  the project and version at the top of the compiled script
      */
     grunt.extendConfig({
-        clean: {
-            www_dev: ['www/*']
+        uglify: {
+            options: {
+                banner: '//' + package_json.name + ' ' + package_json.version + '\n'
+            },
+            prod: {
+                files: {
+                    'www/app.js': 'www/app.js'
+                }
+            }
         }
     });
 
-    grunt.registerTask('default', ['clean:www_dev', 'melvinjs:dev']);
+    grunt.registerTask('default', ['melvinjs:dev']);
+    grunt.registerTask('prod', ['melvinjs:prod']);
 };
