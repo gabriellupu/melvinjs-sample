fdb65531dce4f1cd404a70a392817f7c35e498ab
Gabriel Lupu <gabriel.lupu@codegile.com>
2017-06-03 12:55:13 +0300

    Remove unneeded dependencies and adjust the proxy

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var http = require('http');
-var httpProxy = require('http-proxy');
 var staticServer = require('node-static');
 
 var proxy = httpProxy.createProxyServer();
@@ -7,12 +6,8 @@ var proxy = httpProxy.createProxyServer();
 var fileServer = new staticServer.Server('./www');
 var fileServerNode = new staticServer.Server('./');
 
-
 var server = http.createServer(function(req, res) {
-    if (req.url.match(/\/api\//)) {
-        proxy.web(req, res, { target: 'http://homi-rest-service.us-west-2.elasticbeanstalk.com' });
-    } else if (req.url.match(/\/node_modules\//)) {
-        console.log('node module', req.url);
+    if (req.url.match(/\/node_modules\//)) {
         req.addListener('end', function() {
             fileServerNode.serve(req, res);
         }).resume();
